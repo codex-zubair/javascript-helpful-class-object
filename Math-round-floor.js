855328792f2322ed4839ae648e5022c04125e4a0
Rocko <codingwithrock@gmail.com>
2022-08-08 18:36:30 +0600

    added math round and premative and not premative stuff

---
+++ Math-round-floor.js
@@ -0,0 +1,7 @@
+console.log(Math.round(5.5));
+console.log(Math.round(5.4));
+console.log("======================")
+console.log(Math.floor(5.98));
+
+
+
---
+++ type-of-variables.js
@@ -0,0 +1,11 @@
+let length = 16;                               //* Number
+let lastName = "Johnson";                      //* String
+let x = {firstName:"John", lastName:"Doe"};    //* Object
+
+
+// *Booleans can only have two values: true or false
+
+
+let x;           //* Now x is undefined
+x = 5;           //* Now x is a Number
+x = "John";      //* Now x is a String
