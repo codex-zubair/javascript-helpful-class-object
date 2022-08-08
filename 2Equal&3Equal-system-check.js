bca12005cd82732a7fb895cff48b860318224792
Rocko <codingwithrock@gmail.com>
2022-08-08 11:47:08 +0600

    == does't care if it's string or not just need to eaqual value but === also check it's string or not now it's cleared

---
+++ 2Equal&3Equal-system-check.js
@@ -0,0 +1,18 @@
+
+
+
+
+var x = 10;
+const y = '10';
+
+
+if (x == y) {
+    console.log("working fine");
+}
+
+
+
+
+if (x === y) {
+    console.log("For === Check also it's string type or not.");
+}
