b16ad29bb32a9cc8e17b02a419c847c03596596a
Rocko <codingwithrock@gmail.com>
2022-08-08 11:39:18 +0600

    switch system checked and it's worked fine

---
+++ switch-system.js
@@ -0,0 +1,28 @@
+
+
+function switchTest(value) {
+    switch (value) {
+        case (10):
+            {
+                console.log("it's ten 1");
+
+            }
+        case (10):
+            {
+                console.log("it's ten 2");
+                break
+            }
+        case (10):
+            {
+                console.log("it's ten");
+            }
+        
+        default:
+            {
+                console.log("value not correct");
+            }
+    }
+}
+
+
+switchTest(1);
