c04c2967abc4918fd567b4724b9cc8bb81c1d90c
Rocko <codingwithrock@gmail.com>
2022-08-09 22:39:38 +0600

    arrow function understand and added the arrow function system

---
+++ arrow-function.js
@@ -0,0 +1,29 @@
+
+// * arrow function system
+let sum = () => {
+    console.log("Arrow Function");
+
+    return 1 + 5;
+}
+
+
+// * Normal Function system
+function normalFunction() {
+    console.log("Normal Function");
+}
+
+
+normalFunction();
+console.log(sum());
+
+
+
+
+
+
+
+
+
+
+
+
--- type-of-variables.js
+++ type-of-variables.js
@@ -6,6 +6,6 @@ let x = {firstName:"John", lastName:"Doe"};    //* Object
 // *Booleans can only have two values: true or false
 
 
-let x;           //* Now x is undefined
+// let x;           //* Now x is undefined
 x = 5;           //* Now x is a Number
 x = "John";      //* Now x is a String
