7d875b8805686985ee070ae07ba97e54b75a1743
Rocko <codingwithrock@gmail.com>
2022-08-09 13:15:01 +0600

    math random and math max are added

--- Math-round-floor.js
+++ Math-round-floor.js
@@ -4,6 +4,28 @@ console.log("======================")
 console.log(Math.floor(5.98));
 console.log(Math.ceil(5.98));
 
+i = 3;
+
+console.log(Math.min(34, 343, 454, 0));
 
 
-console.log90sdfsdkjfl()()dfklj;
+let sum = 0;
+for (let i = 0; i <= 3; i++) {
+    sum = sum + i;
+}
+
+console.log(sum);
+
+
+
+
+const a = [2, 232, 3, 23, 2, 32, 3, 2];
+
+
+
+for (as of a) {
+    console.log(as);
+}
+
+
+
---
+++ math-max.js
@@ -0,0 +1,4 @@
+const arr = [123, 232, 34, 45, 56, 565, 56, 5675, 6];
+
+
+console.log(Math.max(...arr));
