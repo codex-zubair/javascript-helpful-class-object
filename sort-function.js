7c9fd5d1f05975909f761a0c867904babf7aaf5f
Rocko <codingwithrock@gmail.com>
2022-08-09 13:49:38 +0600

    sorted system are cleared for later is directly but for function need some modification

---
+++ sort-function.js
@@ -0,0 +1,13 @@
+const values = [12323, 234, 234, 3, 43, 4, 233, 323, 3, 54, 34, 5, 65, 8, 32, 312, 1];
+
+values.sort(function (a, b) { return a - b });
+
+console.log(values);
+
+
+// * For Later simple
+
+alpa = ['a', 'f', 'd', 'c']
+
+console.log(alpa.sort());
+
